52ab7dd369f7396d79cf257fb98df5b273628d29
matthieu.napoli <matthieu.napoli@e8506dc9-fcca-43ba-bdf7-415c996b1c3c>
2011-03-10 16:00:44 +0000

    ref #2344

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -14,7 +14,22 @@ class IndexController extends Core_Controller
 
     public function phpinfoAction()
     {
+    }
 
+    public function validationAction()
+    {
+        $this->view->command = "php /home/dev/library/Core/trunk/scripts/validator/validator.php"
+            ." /home/dev/library/AF"
+            ." /home/dev/library/Calc"
+            ." /home/dev/library/Core"
+            ." /home/dev/library/Export"
+            ." /home/dev/library/TEC"
+            ." /home/dev/library/UI"
+            ." /home/dev/library/Unit"
+            ." /home/dev/library/User";
+        $output = array();
+        exec($this->view->command, $output, $retour);
+        $this->view->errors = implode("\n", $output);
     }
 
 }
